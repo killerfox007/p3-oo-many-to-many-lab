8d9d99593a28eb8a13578cfb038d686e20673632
Kaleb Fox <kalebfox7@yahoo.com>
2024-07-01 20:18:28 -0400

    done

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,65 @@
 class Author:
-    pass
+    all =[]
+    
 
+    def __init__(self, name):
+        self._contracts=[]
+        self._books=[]
+        if isinstance(name, str):
+            self.name = name
+        else: 
+            raise Exception()
+    
+    def sign_contract(self, book, date, royalties):
+       return Contract(self,book,date,royalties)
+    def contracts(self):
+        return self._contracts
+    def books(self):
+        return self._books
 
+    def total_royalties(self):
+        x = [contract.royalties for contract in self._contracts]
+        return sum(x)
 class Book:
-    pass
+    all =[]
+    def __init__(self, title):
+        self._contracts = []
+        self._authors = []
+        if isinstance(title, str):
+            self.title = title
+        else: 
+            raise Exception()
+    def contracts(self):
+        return self._contracts
+    def authors(self):
+        return self._authors
 
-
+        
+        
 class Contract:
-    pass
+    all = []
+    def __init__(self, author, book, date, royalties):
+        if isinstance(author, Author):
+            self.author = author
+        else: 
+            raise Exception()
+        if isinstance(book, Book):
+            self.book = book
+        else: 
+            raise Exception()
+        if isinstance(date, str):
+            self.date = date
+        else: 
+            raise Exception()
+        if isinstance(royalties, int):
+            self.royalties = royalties
+        else: 
+            raise Exception()
+        author.contracts().append(self)
+        author.books().append(book)
+        book.contracts().append(self)
+        book.authors().append(author)
+        Contract.all.append(self)
+    @classmethod
+    def contracts_by_date(cls,date):
+        return [contract for contract in cls.all if date == contract.date] 
